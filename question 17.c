dee28caaea67bc8e28980abf99ddeaf4b8eb1c7c
deepu2727 <et24bthcs027@kzu.ac.in>
2025-09-06 14:17:37 +0530

    Create question 17.c

---
+++ question 17.c
@@ -0,0 +1,29 @@
+// Write a program that reads a matrix and displays the sum of the elements below the main diagonal (Hint: Calculate the sum of elements Aij where i>j)
+#include <stdio.h>
+
+int main() {
+    int n, i, j, sum = 0;
+
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    printf("Enter elements of %d x %d matrix:\n", n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Calculate sum of elements below main diagonal (i > j)
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < i; j++) {
+            sum += a[i][j];
+        }
+    }
+
+    printf("Sum of elements below main diagonal = %d\n", sum);
+
+    return 0;
+}
